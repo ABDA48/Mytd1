5bc35c8cd727ce46a24325b082dce20ab215e286
ABDA48 <42075569+ABDA48@users.noreply.github.com>
2020-07-29 14:59:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-
+if(process.env.NODE_ENV!=='production'){
+    require('dotenv').parse()
+}
 const express=require('express')
 const app=express()
 const expressLayout=require('express-ejs-layouts')
 const indexRouter=require('./routes/index')
-const { MongoClient } = require('mongodb')
-const mongodb=require('mongodb').MongoClient
-var url='mongodb://localhost:27017/mybrary'
-MongoClient.connect(url,(err,db)=>{
-if(err) throw err;
-var dbo=db.db('mybrary');
-console.log('connected');
-});
+const mongoose=require('mongoose')
+mongoose.connect(process.env.Database_URL,{useNewUrlParser:true})
+const db=mongoose.connection
+db.on('error',error=>console.error(error))
+db.once('open',()=>console.log('Connected to mongoose'))
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
 app.set('layout','layouts/layout')
